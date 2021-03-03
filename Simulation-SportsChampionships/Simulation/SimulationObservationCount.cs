1dd38796069c76fdeee9d1d58ec919dcbf33e397
bartczernicki <bartczernicki@outlook.com>
2021-03-02 19:08:37 -0500

    Fixed step

---
+++ Simulation-SportsChampionships/Simulation/SimulationObservationCount.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimulationSportsChampionships.Simulation
+{
+    public class SimulationObservationCount
+    {
+        public int Observation { get; set; }
+        public int Count { get; set; }
+    }
+}
